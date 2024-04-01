6b4860b04c2cecf962809a1d9c6fd23c2d84ed26
Santicm23 <Santicastrom@outlook.com>
2024-04-01 10:54:43 -0500

    fixing bugs

--- pqrs/views.py
+++ pqrs/views.py
@@ -1,20 +1,11 @@
-from django.shortcuts import render
-
-from django.views import View
-from django.utils.decorators import method_decorator
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from config.settings import SECRET_KEY
-from rest_framework.exceptions import AuthenticationFailed
-from django.http import HttpResponse
-
-import jwt
-import datetime
 import json
-import pika
+
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 from pqrs.rabbitmq import get_rabbitmq_connection
-
 
 from .models import PQR
 
@@ -24,13 +15,12 @@ def send_jwt_validation_request(token):
     channel = connection.channel()
 
     # Define el mensaje que enviarás, puede ser cualquier información necesaria para la validación
-    message = {
-        "token": token
-    }
+    message = {"token": token}
 
     # Publica el mensaje en la cola
     channel.basic_publish(
-        exchange='', routing_key='jwt_validation_queue', body=json.dumps(message))
+        exchange="", routing_key="jwt_validation_queue", body=json.dumps(message)
+    )
 
     connection.close()
 
@@ -42,24 +32,23 @@ class createPQRview(View):
 
     def post(self, request):
         jd = json.loads(request.body)
-        token = jd['token']
-        #send_jwt_validation_request(token)
+        token = jd["token"]
+        # send_jwt_validation_request(token)
         try:
             PQR.objects.create(
-                empresa_id=jd['empresa_id'],
-                tipo_solicitud=jd['tipo_solicitud'],
-                estado=jd['estado'],
-                fecha_solicitud=jd['fecha_solicitud'],
-                asunto=jd['asunto'],
-                descripcion=jd['descripcion']
+                empresa_id=jd["empresa_id"],
+                tipo_solicitud=jd["tipo_solicitud"],
+                estado=jd["estado"],
+                fecha_solicitud=jd["fecha_solicitud"],
+                asunto=jd["asunto"],
+                descripcion=jd["descripcion"],
             )
-            return JsonResponse({'message': 'PQR created successfully'}, status=201)
+            return JsonResponse({"message": "PQR created successfully"}, status=201)
         except Exception as e:
-            return JsonResponse({'message': str(e)}, status=400)
-
+            return JsonResponse({"message": str(e)}, status=400)
 
 
 class getPQRview(View):
     def get(self, request, pk):
         pqr = PQR.objects.filter(empresa_id=pk).values()
-        return JsonResponse({'pqr': list(pqr)}, safe=False)
+        return JsonResponse({"pqr": list(pqr)}, safe=False)
