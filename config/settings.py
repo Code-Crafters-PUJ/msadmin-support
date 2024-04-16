42ffaeb00ee22745084e6766ea0eb9066ab95304
Santicm23 <Santicastrom@outlook.com>
2024-04-16 18:47:12 -0500

    fixing environment setup

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+VERSION = "0.1.1"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&^_bgiv)@)&b1!ymc3h^w76l7&%+256qrcxg-qfokn*t(chunx"
+SECRET_KEY = os.getenv("JWT_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "pqrs"
+    "pqrs",
 ]
 
 MIDDLEWARE = [
@@ -100,10 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-RABBITMQ_HOST = 'localhost'
+RABBITMQ_HOST = "localhost"
 RABBITMQ_PORT = 5672
-RABBITMQ_USERNAME = 'usuario'
-RABBITMQ_PASSWORD = 'adsadsdasasdasd'
+RABBITMQ_USERNAME = "usuario"
+RABBITMQ_PASSWORD = "adsadsdasasdasd"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
--- pqrs/views.py
+++ pqrs/views.py
@@ -1,21 +1,24 @@
 import json
+from typing import Any
 
-from django.http import JsonResponse
+import jwt
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
+from config.settings import SECRET_KEY
 from pqrs.rabbitmq import get_rabbitmq_connection
 
 from .models import PQR
 
 
-def send_jwt_validation_request(token):
+def send_jwt_validation_request(token: str) -> None:
     connection = get_rabbitmq_connection()
     channel = connection.channel()
 
     # Define el mensaje que enviarás, puede ser cualquier información necesaria para la validación
-    message = {"token": token}
+    message: dict[str, Any] = {"token": token}
 
     # Publica el mensaje en la cola
     channel.basic_publish(
@@ -25,16 +28,49 @@ def send_jwt_validation_request(token):
     connection.close()
 
 
+def validate_role(token: str, role: str) -> bool:
+    payload: dict[str, Any]
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        print(payload)
+    except jwt.InvalidTokenError:
+        return False
+
+    if payload["role"] != role:
+        return False
+    return True
+
+
+def validate_admin_role(token: str) -> bool:
+    return validate_role(token, "ADMIN")
+
+
+def validate_soporte_role(token: str) -> bool:
+    return validate_role(token, "SOPORTE")
+
+
 class createPQRview(View):
     @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        token: str = request.headers.get("Authorization") or ""
+
+        if not token:
+            return JsonResponse(
+                {"message": "You must include an Authorization header"}, status=401
+            )
+
         jd = json.loads(request.body)
-        token = jd["token"]
         # send_jwt_validation_request(token)
+
         try:
+            if not (validate_admin_role(token) and validate_soporte_role(token)):
+                return JsonResponse(
+                    {"message": "You don't have the required permissions"}, status=403
+                )
+
             PQR.objects.create(
                 empresa_id=jd["empresa_id"],
                 tipo_solicitud=jd["tipo_solicitud"],
@@ -49,6 +85,18 @@ class createPQRview(View):
 
 
 class getPQRview(View):
-    def get(self, request, pk):
+    def get(self, request: HttpRequest, pk: int) -> JsonResponse:
+        token: str = request.headers.get("Authorization") or ""
+
+        if not token:
+            return JsonResponse(
+                {"message": "You must include an Authorization header"}, status=401
+            )
+
+        if not (validate_admin_role(token) and validate_soporte_role(token)):
+            return JsonResponse(
+                {"message": "You don't have the required permissions"}, status=403
+            )
+
         pqr = PQR.objects.filter(empresa_id=pk).values()
         return JsonResponse({"pqr": list(pqr)}, safe=False)
